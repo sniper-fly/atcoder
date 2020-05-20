7ae8b928cb500ef34dce99fc905b1d1ae94e8c1e
wata <ryo.nakai0@gmail.com>
2020-05-20 15:46:51 +0900

    168 resolved

--- ABC/168/clon.cpp
+++ ABC/168/clon.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdio>
 #include <cmath>
 using namespace std;
 
@@ -13,12 +14,12 @@ int		main(void)
 	// - 2*a*b*( sin(M_PI * m / 30) * sin(M_PI * h / 6)
 	// 		+ (cos(M_PI * m / 30) * cos(M_PI * h / 6)) ) );
 	// ans = sqrt(pow( ( a*sin(M_PI * m / 30) - b*sin(M_PI * h / 6) ), 2.0) + pow((a * cos(M_PI * m / 30) - b * cos(M_PI * h / 6)), 2.0) );
-	a_x = a * sin(30 * h);
-	a_y = a * cos(30 * h);
-	b_x = b * sin(6 * m);
-	b_y = b * cos(6 * m);
+	a_x = a * sin(2 * M_PI * (60 * h + m) / 720);
+	a_y = a * cos(2 * M_PI * (60 * h + m) / 720);
+	b_x = b * sin(2 * M_PI * m / 60);
+	b_y = b * cos(2 * M_PI * m / 60);
 	ans = sqrt(pow((a_x - b_x), 2.0) + pow((a_y - b_y), 2.0));
 	
-	cout << ans << endl;
+	printf("%.10f\n", ans);
 	return (0);
 }
