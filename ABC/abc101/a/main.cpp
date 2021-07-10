a92972c7e48ce07e0986d164572c386cddecb06c
rnakai <rnakai@student.42tokyo.jp>
2021-07-10 14:40:33 +0900

    acc, ojコマンドの追加

---
+++ ABC/abc101/a/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using ld=long double;
+using pll=pair<ll, ll>;
+#define rep(i,n) for (ll i=0; i<n; ++i)
+#define all(c) begin(c),end(c)
+#define PI acos(-1)
+#define oo 2e18
+template<typename T1, typename T2>
+bool chmax(T1 &a,T2 b){if(a<b){a=b;return true;}else return false;}
+template<typename T1, typename T2>
+bool chmin(T1 &a,T2 b){if(a>b){a=b;return true;}else return false;}
+//priority_queue<ll, vector<ll>, greater<ll>> Q;
+// LLMAX = 9,223,372,036,854,775,807 (9.2 * 10^18)
+// IMAX = 2147483647 (2.1*10^9)
+/*
+
+*/
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    cout << fixed << setprecision(10);
+
+    string N;
+    cin >> N;
+    ll plus = 0,
+    minus = 0;
+    rep(i, 4) {
+        if (N[i] == '+'){
+            plus++;
+        }
+        else if (N[i] == '-') {
+            minus--;
+        }
+    }
+    cout << plus + minus << endl;
+}
