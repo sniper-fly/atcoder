e88a8271d3f6e3ad4a779233358f06712bc7faa1
rnakai <rnakai@student.42tokyo.jp>
2021-08-21 21:35:14 +0900

    abc215

---
+++ ABC/abc215/b/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using ld=long double;
+using pll=pair<ll, ll>;
+#define rep(i,n) for (ll i=0; i<n; ++i)
+#define all(c) begin(c),end(c)
+#define PI acos(-1)
+#define oo 2e18
+template<typename T1, typename T2>
+bool chmax(T1 &a,T2 b){if(a<b){a=b;return true;}else return false;}
+template<typename T1, typename T2>
+bool chmin(T1 &a,T2 b){if(a>b){a=b;return true;}else return false;}
+//priority_queue<ll, vector<ll>, greater<ll>> Q;
+// LMAX = 18446744073709551615 (1.8*10^19)
+// IMAX = 2147483647 (2.1*10^9)
+/*
+
+*/
+
+int main()
+{
+    ll N;
+    cin >> N;
+    ll k;
+    k = (ll)log2l(N);
+    std::cout << k << std::endl;
+}
